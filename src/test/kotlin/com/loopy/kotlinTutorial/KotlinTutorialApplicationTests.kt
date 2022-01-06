41de1500095c8a64445a1f0d887bd539713160e3
seongkyu-lim <96x60812@naver.com>
2022-01-06 23:08:08 +0900

    start

---
+++ src/test/kotlin/com/loopy/kotlinTutorial/KotlinTutorialApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.loopy.kotlinTutorial
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinTutorialApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
