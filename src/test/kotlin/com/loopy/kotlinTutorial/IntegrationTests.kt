442fd7ed800affd7b1ce09b5acb8a8958cc03c3f
seongkyu-lim <96x60812@naver.com>
2022-01-06 23:29:49 +0900

    add: own extensions

---
+++ src/test/kotlin/com/loopy/kotlinTutorial/IntegrationTests.kt
@@ -0,0 +1,48 @@
+package com.loopy.kotlinTutorial
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.AfterAll
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.boot.test.web.client.getForEntity
+import org.springframework.http.HttpStatus
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class IntegrationTests(@Autowired val restTemplate: TestRestTemplate) {
+
+    @BeforeAll
+    fun setup() {
+        println(">> Setup")
+    }
+
+    @Test
+    fun `Assert blog page title, content and status code`() {
+        println(">> Assert blog page title, content and status code")
+        val entity = restTemplate.getForEntity<String>("/")
+        assertThat(entity.statusCode).isEqualTo(HttpStatus.OK)
+        assertThat(entity.body).contains("<html>\n" +
+                "<head>\n" +
+                "    <title>blog</title>\n" +
+                "</head>\n" +
+                "<body>\n" +
+                "\n" +
+                "<h1>blog</h1>\n" +
+                "\n" +
+                "</body>\n" +
+                "</html>")
+    }
+
+    @Test
+    fun `Assert article page title, content and status code`() {
+        println(">> TODO")
+    }
+
+    @AfterAll
+    fun teardown() {
+        println(">> Tear down")
+    }
+
+}
